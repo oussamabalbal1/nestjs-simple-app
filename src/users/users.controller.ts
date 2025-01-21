448535cfd259daeae29e7ec2511bd8919d98cf93
unknown <o.balbal@esi-sba.dz>
2025-01-21 10:59:31 -0500

    fix some issues

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDTO } from './DTO/user.create.dto';
 
@@ -11,10 +11,21 @@ export class UsersController {
     createOneUser(@Body() user:CreateUserDTO){
         return this.userservice.createOneUser(user)
     }
+
+
+    // @Get('users')
+    // @Render('users')
+    // listUsers() {
+    //   return { users: this.users };
+    // }
+
     //get all users
     @Get()
+    @Render('users')
     GetAllUsers(){
-        return this.userservice.getAllUsers()
+        return { users: this.userservice.getAllUsers() }
     }
+
+    
 
 }
