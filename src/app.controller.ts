560822d3327d53e31d1abdc0623d0a24d1b9e3c0
unknown <o.balbal@esi-sba.dz>
2025-01-21 08:26:57 -0500

    my first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,39 @@
+
+import { Get, Controller, Render, Post, Body } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+
+  private users = [];
+  @Get('register')
+  @Render('register')
+  showRegistrationForm() {
+    return {}; // No data needed for the form initially
+  }
+
+    // Handle form submission
+  @Post('register')
+  registerUser(@Body() body: { username: string; email: string; password: string }) {
+      const { username, email, password } = body;
+  
+      // Add user to the array
+      this.users.push({ username, email, password });
+  
+      // Log the users (for debugging purposes)
+      console.log('Registered Users:', this.users);
+  
+      return { message: 'User registered successfully!', users: this.users };
+  }
+    
+  @Get('users')
+  @Render('users')
+  listUsers() {
+    return { users: this.users };
+  }
+  
+  @Get()
+  @Render('index')
+  root() {
+    return { message: 'Hello world!',name:'oussama',list: ['oussama','ali','abbes'] };
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Set up static assets and views directory
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(3000);
+  console.log('Application is running on http://localhost:3000');
+}
+bootstrap();
