d47d2c47edb673e0237e6f505f0f0a2b3c01a009
oussamabalbal <46396011+oussamabalbal1@users.noreply.github.com>
2025-07-09 06:12:37 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
-import { PrometheusService } from './monitoring/prometheus.service';
-import { PrometheusInterceptor } from './monitoring/prometheus.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,8 +12,7 @@ async function bootstrap() {
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
 
-  const prometheusService = app.get(PrometheusService);
-  app.useGlobalInterceptors(new PrometheusInterceptor(prometheusService));
+
   
   await app.listen(3000,'0.0.0.0');
   console.log('Application is running on http://localhost:3000');
